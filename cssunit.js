fed365aa3b00ac905ea5e36eca59c5f2354c2230
gagarine <perdrisat@gmail.com>
2010-09-26 21:59:40 +0200

    start the plugin

--- cssunit.js
+++ cssunit.js
@@ -1,31 +1,56 @@
 /* 
  */
 
- $(document).ready(function() {
-   //create the export UI
-   $('body').append('<div id="cssunit"><h3>CSSunit<a href="#">open</a></h3><div class="content">asdf</div></div>');
 
-   $('#cssunit h3 a').click(function(event){
-     $('#cssunit .content').slideToggle('fast');
-   }
-
+//this code need to be move in function
+$(document).ready(function() {
+  //create the export UI
+  $('body').append('<div id="cssunit"><h3>CSSunit<a href="#">open</a></h3><div class="content"></div></div>');
+  var cssunit = $('#cssunit');
+  var menu =  '<ul>'
+  + '<li><a href="#" class="track">start-selection</a></li>'
+  + '<li><a href="#" class="untrack">stop-selection</a></li>'
+  + '<li><a href="#" class="export">export test function</a></li>'
+  + '</ul>';
+ $('.content',cssunit).append(menu);
+  $('h3 a',cssunit).click(function(event){
+    $('#cssunit .content').slideToggle('fast');
+  }
   );
 
-   //element select firebug like
-   $('body').not('#cssunit') //FIXME not doesn't work
-     .mouseover(function(event) {
-       $(event.target).addClass('cssunit-outline-element');
-     })
-     .mouseout(function(event) {
-       $(event.target).removeClass('cssunit-outline-element');
-     })
-     .click(function(event) {
-       $(event.target).toggleClass('cssunit-outline-element-selected');
-     });
- });
+  $('.track',cssunit).click(function(event){
+    $('body:not(#cssunit)').outline('start'); //FIXME 'not' don't work
+  });
+  
+  $('.untrack',cssunit).click(function(event){
+    $('body:not(#cssunit)').outline('stop'); //FIXME 'not' don't work
+  });
+  
+});
 
 
 
 
 
 
+(function( $ ){
+  
+  //Select element like firebug
+  $.fn.outline = function(statu) {
+    return this.each(function() {
+      var $this = $(this);
+      if ( !statu || statu == 'start' ) {
+        $this.bind('mouseover.cssunit mouseout.cssunit', function(event) {
+          $(event.target).toggleClass('cssunit-outline-element');
+        });
+
+        $this.bind('click.cssunit',function(event) {
+          $(event.target).toggleClass('cssunit-outline-element-selected');
+        });
+      }
+      if ( !statu || statu == 'stop' ) {
+        $this.unbind('.cssunit');
+      }
+    });
+  };
+})( jQuery );
