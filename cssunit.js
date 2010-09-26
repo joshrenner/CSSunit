e3d6216fa3e3d15b36e9aa0b22efd7ed7b0f221b
gagarine <perdrisat@gmail.com>
2010-09-26 22:53:38 +0200

    move cssComputed

--- cssunit.js
+++ cssunit.js
@@ -1,7 +1,3 @@
-/* 
- */
-
-
 //this code need to be move in function
 $(document).ready(function() {
   //create the export UI
@@ -12,7 +8,7 @@ $(document).ready(function() {
   + '<li><a href="#" class="untrack">stop-selection</a></li>'
   + '<li><a href="#" class="export">export test function</a></li>'
   + '</ul>';
- $('.content',cssunit).append(menu);
+  $('.content',cssunit).append(menu);
   $('h3 a',cssunit).click(function(event){
     $('#cssunit .content').slideToggle('fast');
   }
@@ -28,13 +24,10 @@ $(document).ready(function() {
   
 });
 
-
-
-
-
-
+/*
+ * cssunit plugin
+ */
 (function( $ ){
-  
   //Select element like firebug
   $.fn.outline = function(statu) {
     return this.each(function() {
@@ -53,4 +46,27 @@ $(document).ready(function() {
       }
     });
   };
+
+  //
+  $.fn.cssComputed = function(){
+    var attr = ['font-family','font-size','font-weight','font-style','color',
+    'text-transform','text-decoration','letter-spacing','word-spacing',
+    'line-height','text-align','vertical-align','direction','background-color',
+    'background-image','background-repeat','background-position',
+    'background-attachment','opacity','width','height','top','right','bottom',
+    'left','margin-top','margin-right','margin-bottom','margin-left',
+    'padding-top','padding-right','padding-bottom','padding-left',
+    'border-top-width','border-right-width','border-bottom-width',
+    'border-left-width','border-top-color','border-right-color',
+    'border-bottom-color','border-left-color','border-top-style',
+    'border-right-style','border-bottom-style','border-left-style','position',
+    'display','visibility','z-index','overflow-x','overflow-y','white-space',
+    'clip','float','clear','cursor','list-style-image','list-style-position',
+    'list-style-type','marker-offset'];
+    var len = attr.length, obj = {};
+    for (var i = 0; i < len; i++){
+      obj[attr[i]] = jQuery.fn.css.call(this, attr[i]);
+    }
+    return obj;
+  }
 })( jQuery );
--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-  var css_text = $('body').getCssAll();
+  var css_text = $('body').cssComputed();
   console.log(css_text);
   //$('#test-me-log').html(css_text);
   //var title = $('h2');
@@ -37,27 +37,3 @@ test("basics", function() {
 });
 */
 
-
-/*
- * Get all the computed style of an element
- */
-jQuery.fn.getCssAll = function() {
-    var attr = ['font-family','font-size','font-weight','font-style','color',
-        'text-transform','text-decoration','letter-spacing','word-spacing',
-        'line-height','text-align','vertical-align','direction','background-color',
-        'background-image','background-repeat','background-position',
-        'background-attachment','opacity','width','height','top','right','bottom',
-        'left','margin-top','margin-right','margin-bottom','margin-left',
-        'padding-top','padding-right','padding-bottom','padding-left',
-        'border-top-width','border-right-width','border-bottom-width',
-        'border-left-width','border-top-color','border-right-color',
-        'border-bottom-color','border-left-color','border-top-style',
-        'border-right-style','border-bottom-style','border-left-style','position',
-        'display','visibility','z-index','overflow-x','overflow-y','white-space',
-        'clip','float','clear','cursor','list-style-image','list-style-position',
-        'list-style-type','marker-offset'];
-    var len = attr.length, obj = {};
-    for (var i = 0; i < len; i++)
-        obj[attr[i]] = jQuery.fn.css.call(this, attr[i]);
-    return obj;
-}
